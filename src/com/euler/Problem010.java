2208487939337cff1653783ffb96075c514e1bf5
Alexander.Wolf <Alexander.D.Wolf@gmail.com>
2015-12-14 16:24:01 -0600

    Add solutions 1-5

--- src/com/euler/Problem010.java
+++ src/com/euler/Problem010.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  *                             20 is 19 + 17 + 13 + 11 + 17 = 77?
  * Find the sum of all the primes below two million.
- * A:142913828922 found 12/11/15
+ *
  */
-public class ProblemTen {
+public class Problem010 {
 
 
     public static void main(String[] args) {
